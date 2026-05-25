e2064650b73c1925e5cf1f2d7eaffed3fae6f50f
yoongles7 <pritikas820@gmail.com>
2026-05-26 01:07:31 +0545

    index page done

---
+++ app/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hey Extras!!! This is the ulitmate appreciation page for the Great Explosion Murder God Dynamight!")
