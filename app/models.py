11e77a0b4209fe1c439dd59c400b5778119337a8
yoongles7 <pritikas820@gmail.com>
2026-05-29 23:26:20 +0545

    first model of the app Details

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Details(models.Model):
+    age = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.age
--- app/views.py
+++ app/views.py
@@ -3,14 +3,20 @@ from django.http import HttpResponse, JsonResponse
 def index(request):
     return HttpResponse("Hey Extras!!! This is the ulitmate appreciation page for the Great Explosion Murder God Dynamight!")
 
-
-def general_details(self):
+def general_details(request):
     data = {
         'name': 'Bakugo Katsuki',
         'hero_name': 'Great Explosion Murder God Dynamight (Daibaku Kisshin Dainamaito)',
+        'quirk': 'Explosion',
         'birthday': 'April 20',
+        'birthplace': 'near Shizuoka Prefecture',
         'manga/anime': 'My Hero Academia (Boku no Hiro Akademia)',
         'author/creator': 'Kohei Horikoshi',
-        'role': 'Supporting character (Deuteragonist)'
+        'role': 'Supporting character (Deuteragonist)',
+        'Mother': 'Bakugo Mitsuki',
+        'Father': 'Bakugo Masaru',
+        'Blood Type': 'A',
+        'Japanese VA': 'Nobuhiko Okamoto',
+        'English VA': 'Clifford Chapin',
     }
     return JsonResponse(data)
