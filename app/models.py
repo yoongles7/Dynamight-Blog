e2709adf28f72390d564babefa31256fa69785be
yoongles7 <pritikas820@gmail.com>
2026-06-03 01:14:45 +0545

    learned about different model fields including jsonfield

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Detail(models.Model):
-    age = models.CharField(max_length=200)
+    detail = models.JSONField(default=dict)
     
     def __str__(self):
-        return self.age
+        return str(self.detail)
--- app/views.py
+++ app/views.py
@@ -1,10 +1,18 @@
 from django.http import HttpResponse, JsonResponse
+from django.forms.models import model_to_dict
+
+from .models import Detail
 
 def index(request):
     return HttpResponse("Hey Extras!!! This is the ulitmate appreciation page for the Great Explosion Murder God Dynamight!")
 
 def general_details(request):
-    data = {
+    details = Detail.objects.all()
+    data1 = {}
+    for d in details:
+        data1.update(d.detail)
+        
+    data2 = {
         'name': 'Bakugo Katsuki',
         'hero_name': 'Great Explosion Murder God Dynamight (Daibaku Kisshin Dainamaito)',
         'quirk': 'Explosion',
@@ -13,10 +21,14 @@ def general_details(request):
         'manga/anime': 'My Hero Academia (Boku no Hiro Akademia)',
         'author/creator': 'Kohei Horikoshi',
         'role': 'Supporting character (Deuteragonist)',
+        'occupation': 'Pro Hero',
         'Mother': 'Bakugo Mitsuki',
         'Father': 'Bakugo Masaru',
         'Blood Type': 'A',
         'Japanese VA': 'Nobuhiko Okamoto',
         'English VA': 'Clifford Chapin',
+        'fighthing_style': 'close-ranged combat',
+        'education': ['Aldera Junior High', 'U.A. High School']
     }
+    data = {**data1, **data2}
     return JsonResponse(data)
