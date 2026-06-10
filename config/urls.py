2d126eb89136cce8529c4b30cf0d3846c3c1babb
yoongles7 <pritikas820@gmail.com>
2026-06-11 00:44:15 +0545

    health-check endpoint (django-health-check)

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from health_check.views import HealthCheckView
 
 urlpatterns = [
     path("", include("apps.blog.urls")),
     path('admin/', admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
+    path("health/", HealthCheckView.as_view()),
 ]
