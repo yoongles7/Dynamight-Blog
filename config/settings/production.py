3c1a0bc1f741997062a81d4f40322520dc3c1bce
yoongles7 <pritikas820@gmail.com>
2026-06-10 17:10:58 +0545

    debug logic fix

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 import os
 
 # Production settings
-DEBUG = os.environ.get('DEBUG', 'False') == 'False'
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 # Security settings
 SECRET_KEY = os.environ.get('SECRET_KEY')
